93d9d3511563f00ecf616cb78167dab4fc7df87a
Bektur777 <ubei003@mail.ru>
2024-07-18 09:46:39 +0600

    Refactor code

--- src/main/java/kg/bektur/todoapp/controller/task/TasksController.java
+++ src/main/java/kg/bektur/todoapp/controller/task/TasksController.java
@@ -1,7 +1,6 @@
 package kg.bektur.todoapp.controller.task;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
--- src/main/java/kg/bektur/todoapp/dto/TaskDto.java
+++ src/main/java/kg/bektur/todoapp/dto/TaskDto.java
@@ -1,7 +1,5 @@
 package kg.bektur.todoapp.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import kg.bektur.todoapp.enumuration.TaskStatus;
 import lombok.*;
 
