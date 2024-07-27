8df0d86e5ae85fefbfbc3449d8860f57293d571a
Bektur777 <ubei003@mail.ru>
2024-07-27 13:35:39 +0600

    Add user details service

--- src/main/java/kg/bektur/todoapp/security/UserDetailsServiceImpl.java
+++ src/main/java/kg/bektur/todoapp/security/UserDetailsServiceImpl.java
@@ -1,10 +1,17 @@
-package kg.bektur.todoapp.service.impl;
+package kg.bektur.todoapp.security;
 
+import kg.bektur.todoapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
