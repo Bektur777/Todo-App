74d088555a6c3e334e05deb4ece2dc8a33151737
Bektur777 <ubei003@mail.ru>
2024-07-27 13:35:28 +0600

    Add custom user details

--- src/main/java/kg/bektur/todoapp/config/UserDetailsImpl.java
+++ src/main/java/kg/bektur/todoapp/config/UserDetailsImpl.java
@@ -1,24 +1,31 @@
 package kg.bektur.todoapp.config;
 
+import kg.bektur.todoapp.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
+@RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole().toString()));
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getUsername();
     }
 }
