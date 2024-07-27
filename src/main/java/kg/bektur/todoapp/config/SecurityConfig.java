d5985bfcece81eb750e0259879654aff7c442f39
Bektur777 <ubei003@mail.ru>
2024-07-27 18:06:05 +0600

    Add custom user

--- src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
+++ src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
@@ -1,24 +1,33 @@
 package kg.bektur.todoapp.config;
 
+import kg.bektur.todoapp.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl detailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(httpRequest ->
-                        httpRequest.requestMatchers("/api/tasks/**")
-                                .hasRole("SERVICE"))
+                .authorizeHttpRequests(httpRequest -> httpRequest
+                        .requestMatchers("/api/tasks/**").hasAnyRole("USER", "ADMIN")
+                )
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSession -> httpSession
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .userDetailsService(detailsService)
                 .build();
     }
 }
