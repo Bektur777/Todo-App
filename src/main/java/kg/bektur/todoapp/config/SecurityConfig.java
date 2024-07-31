8e75e455819e7c3dbe3e390b4b7fe3ffbe10843c
Bektur777 <ubei003@mail.ru>
2024-07-31 15:43:26 +0600

    Add default authorization settings

--- src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
+++ src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
@@ -2,20 +2,30 @@ package kg.bektur.todoapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(httpRequest -> httpRequest
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.POST).hasAuthority("SCOPE_task_edit")
+                        .requestMatchers(HttpMethod.PATCH, "/api/tasks/**").hasAuthority("SCOPE_task_edit")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/**").hasAuthority("SCOPE_task_edit")
+                        .requestMatchers(HttpMethod.GET).hasAuthority("SCOPE_task_view")
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
