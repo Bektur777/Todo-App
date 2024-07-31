fbfd56fb755fbbb615f6b6194335c6684eb0375f
Bektur777 <ubei003@mail.ru>
2024-07-31 11:27:11 +0600

    Update security config to test

--- src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
+++ src/main/java/kg/bektur/todoapp/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +30,10 @@ public class SecurityConfig {
                 )
                 .userDetailsService(detailsService)
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
