37a5408b271870499ecf9c3ff0e5dc3170aefb57
Bektur777 <ubei003@mail.ru>
2024-08-02 01:55:28 +0600

    Update DefaultService.java by adding authentication user

--- src/main/java/kg/bektur/todoapp/service/DefaultService.java
+++ src/main/java/kg/bektur/todoapp/service/DefaultService.java
@@ -1,16 +1,18 @@
 package kg.bektur.todoapp.service;
 
+import org.springframework.security.core.Authentication;
+
 import java.util.List;
 
 public interface DefaultService<T, C> {
 
-    List<T> findAll();
+    List<T> findAll(Authentication authentication);
 
-    T find(Long id);
+    T find(Long id, Authentication authentication);
 
-    T create(C c);
+    T create(C c, Authentication authentication);
 
-    T update(C c, Long id);
+    T update(C c, Long id, Authentication authentication);
 
-    void deleteById(Long id);
+    void deleteById(Long id, Authentication authentication);
 }
