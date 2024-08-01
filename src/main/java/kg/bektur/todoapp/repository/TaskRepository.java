99cafd4cd973d1c9cf67a64f8e2e950ba013d859
Bektur777 <ubei003@mail.ru>
2024-08-02 01:57:07 +0600

    Add methods with user id

--- src/main/java/kg/bektur/todoapp/repository/TaskRepository.java
+++ src/main/java/kg/bektur/todoapp/repository/TaskRepository.java
@@ -8,11 +8,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Task t SET t.entityStatus = 'DELETED' WHERE t.id = :id")
-    void softDelete(@Param("id") Long id);
+    @Query("UPDATE Task t SET t.entityStatus = 'DELETED' WHERE t.id = :id AND t.userId = :userId")
+    void softDelete(@Param("id") Long id, @Param("userId") UUID userId);
+
+    List<Task> findAllByUserId(UUID userId);
+
+    Optional<Task> findByIdAndUserId(Long id, UUID userId);
 }
