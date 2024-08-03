a0e3a4579cedf68a7e45e214250965fb945a0b2b
Bektur777 <ubei003@mail.ru>
2024-08-03 21:46:32 +0600

    Add security filter chain

--- admin-service/src/main/java/kg/bektur/adminservice/config/SecurityBeans.java
+++ admin-service/src/main/java/kg/bektur/adminservice/config/SecurityBeans.java
@@ -3,9 +3,14 @@ package kg.bektur.adminservice.config;
 import kg.bektur.adminservice.web.client.OAuthHttpHeadersProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityBeans {
@@ -17,5 +22,17 @@ public class SecurityBeans {
                 new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository,
                         authorizedClientService)
         );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(httpRequest ->
+                        httpRequest.anyRequest().permitAll()
+                )
+                .oauth2Client(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
